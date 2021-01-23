9dfbce684e981c02bcbfffe85cc2bc3fcc66bd1a
mute1997 <mute1008@gmail.com>
2021-01-23 12:17:14 +0900

    add ctrl

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|CTL+b |           |CTL+b |------+------+------+------+------+--------|
  * |  SHFT  |   Z  |   X  |   C  |   V  |   B  | +p   |           | +n   |   N  |   M  |   ,  |   .  |  /   |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | LGui | SYMB |                                       | MDIA |      |      |      |      |
+ *   | Ctrl |      |      | LGui | SYMB |                                       | MDIA |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,--------------.       ,---------------.
  *                                        |       |      |       |      |        |
@@ -50,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,     KC_E,    KC_R,     KC_T,   TMUXX,
         KC_ESC,         KC_A,         KC_S,     KC_D,    KC_F,     KC_G,
         KC_LSFT,        KC_Z,         KC_X,     KC_C,    KC_V,     KC_B,   TMUXP,
-        KC_TRNS,        KC_TRNS,      KC_TRNS,  KC_LGUI, MO(SYMB),
+        KC_LCTL,        KC_TRNS,      KC_TRNS,  KC_LGUI, MO(SYMB),
                                                     KC_TRNS,  KC_TRNS,
                                                               KC_TRNS,
                                                KC_SPC,KC_TRNS,KC_TRNS,
         // right hand
              KC_TRNS,      KC_6,   KC_7,  KC_8,    KC_9,   KC_0,    KC_TRNS,
              TMUXC,        KC_Y,   KC_U,  KC_I,    KC_O,   KC_P,    KC_BSLS,
-                           KC_H,   KC_J,  KC_K,    KC_L,   KC_LCTL, KC_BSPC,
+                           KC_H,   KC_J,  KC_K,    KC_L,   KC_RCTL, KC_BSPC,
              TMUXN,        KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_TRNS,
                                    MO(MDIA),KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
              KC_TRNS,KC_TRNS,
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 # write to ergodox
+
 docker build -t qmk_firmware .
+
 docker run -e keymap=default -e keyboard=ergodox_ez --rm -v $(pwd):/qmk:rw qmk_firmware
+
 teensy-loader-cli -mmcu=atmega32u4 -w ergodox_ez_default.hex -v
